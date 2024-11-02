7887ba02ba01e5937083efaf3e382b74aba1c083
Your Name <doug.cha@gmail.com>
2024-11-02 13:25:04 +0900

    initial co-pilot built app

---
+++ lib/fish_card.dart
@@ -0,0 +1,83 @@
+// FILE: lib/fish_card.dart
+
+import 'package:flutter/material.dart';
+
+enum ImageSize { noshow, small, medium, large }
+
+class FishCard extends StatelessWidget {
+  final int index;
+  final double height;
+  final ImageSize imageSize;
+
+  const FishCard({
+    required this.index,
+    this.height = 120,
+    this.imageSize = ImageSize.large,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double imageHeightFactor;
+    switch (imageSize) {
+      case ImageSize.noshow:
+        imageHeightFactor = 0;
+        break;
+      case ImageSize.small:
+        imageHeightFactor = 1 / 3;
+        break;
+      case ImageSize.medium:
+        imageHeightFactor = 2 / 3;
+        break;
+      case ImageSize.large:
+      default:
+        imageHeightFactor = 1;
+        break;
+    }
+
+    double imageWidth = height * 0.75; // Default width to 75% of height
+
+    return Tooltip(
+      message: 'Click for details',
+      child: Card(
+        child: Container(
+          height: height, // Use the height parameter
+          child: Row(
+            children: <Widget>[
+              if (imageSize != ImageSize.noshow) // Conditionally show the image
+                Container(
+                  width: imageWidth,
+                  height: height * imageHeightFactor,
+                  child: Image.asset(
+                    'assets/images/fish_thumbnail.png', // Replace with your image asset
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Fish $index',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Description of the fish. This is a placeholder text to simulate a longer description that spans multiple lines.',
+                        style: TextStyle(fontSize: 14),
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'fish_card.dart'; // Import the new file
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gyu Gai Do',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Gyu Gai Do'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageController _pageController = PageController();
+
+  void _goToFishScanner() {
+    _pageController.jumpToPage(1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          // First screen: current screen
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have entered the greatest sushi app in the world:',
+                  style: TextStyle(fontSize: 30), 
+                ),
+                Image.asset('images/fish_sushi.jpg'),
+              ],
+            ),
+          ),
+          // Second screen: FishScanner
+          FishScanner(),
+          // Third screen: FishFavorites
+          FishFavorites(),
+          // Fourth screen: FishLibrary
+          FishLibrary(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToFishScanner,
+        tooltip: 'Photo Scan a fish',
+        child: const FaIcon(FontAwesomeIcons.fish),
+      ),
+    );
+  }
+}
+
+class FishScanner extends StatelessWidget {
+  const FishScanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fish Scanner'),
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.8,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FaIcon(
+                FontAwesomeIcons.camera,
+                size: 100,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Take photo of fish to identify and add to your favorites',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FishFavorites extends StatelessWidget {
+  const FishFavorites({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fish Favorites'),
+      ),
+      body: ListView(
+        children: List.generate(5, (index) => FishCard(index: index)),
+      ),
+    );
+  }
+}
+
+class FishLibrary extends StatelessWidget {
+  const FishLibrary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fish Library'),
+      ),
+      body: ListView(
+        children: List.generate(20, (index) => FishCard(index: index, height: 80, imageSize: ImageSize.medium)),
+      ),
+    );
+  }
+}
+
